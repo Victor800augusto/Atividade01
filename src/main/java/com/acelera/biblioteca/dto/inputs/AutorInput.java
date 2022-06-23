7b8e7fa4fb64f4d67ffeb92496ccb48467859103
Victor800augusto <victor.augusto800@gmail.com>
2022-06-23 08:31:41 -0300

    Adicionando Sizes para quase todos inputs

--- src/main/java/com/acelera/biblioteca/dto/inputs/AutorInput.java
+++ src/main/java/com/acelera/biblioteca/dto/inputs/AutorInput.java
@@ -1,6 +1,7 @@
 package com.acelera.biblioteca.dto.inputs;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,8 +11,10 @@ import lombok.Setter;
 public class AutorInput {
 
 	@NotNull(message="O nome é obrigatório!")
+	@Size(min = 2, max = 100, message = "O Nome deve conter entre 2 e 100 caracteres!")
 	private String nome;
 	
 	@NotNull(message="A bibliografia é obrigatória!")
+	@Size(min = 1, max = 1000, message = "A bibliografia deve conter entre 1 e 1000 caracteres!")
 	private String bibliografia;
 }
--- src/main/java/com/acelera/biblioteca/dto/inputs/LivroInput.java
+++ src/main/java/com/acelera/biblioteca/dto/inputs/LivroInput.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 public class LivroInput {
 
 	@NotNull(message = "O título é obrigatório!")
+	@Size(min = 2, max = 200, message = "O título deve conter entre 2 e 200 caracteres!")
 	private String titulo;
 
 	@NotNull(message = "O ano de lançamento é obrigatório!")
