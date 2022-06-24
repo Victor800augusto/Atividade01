a76fc0f5ea55d0d6a7f610ff1b321842083c06cb
Victor800augusto <victor.augusto800@gmail.com>
2022-06-24 09:09:33 -0300

    Adicionando mensagem customizada para error MethodArgumentNotValidException

--- src/main/java/com/acelera/biblioteca/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/acelera/biblioteca/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,14 @@
 package com.acelera.biblioteca.handler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -18,6 +25,32 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ProblemExceptionOutput problema = new ProblemExceptionOutput(HttpStatus.NOT_FOUND.value(),
 				exception.getMessage());
 		return new ResponseEntity<ProblemExceptionOutput>(problema, HttpStatus.NOT_FOUND);
+	}
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		String detail = "Um ou mais campos estão inválidos.";
+
+		BindingResult bindingResult = ex.getBindingResult();
+		
+		List<FieldsExceptionOutput> problemObjects = bindingResult.getAllErrors().stream().map(objectError -> {
+			String message = objectError.getDefaultMessage();
+			String name = objectError.getObjectName();
+
+			if (objectError instanceof FieldError) {
+				name = ((FieldError) objectError).getField();
+			}
+
+			message = message.replace("{1}", name);
+			
+			return FieldsExceptionOutput.builder().name(name).message(message).build();
+		}).collect(Collectors.toList());
+
+		ProblemExceptionOutput problema = new ProblemExceptionOutput(HttpStatus.BAD_REQUEST.value(), detail,
+				problemObjects);
+
+		return new ResponseEntity<Object>(problema, HttpStatus.BAD_REQUEST);
 	}
 
 }
--- src/main/java/com/acelera/biblioteca/handler/FieldsExceptionOutput.java
+++ src/main/java/com/acelera/biblioteca/handler/FieldsExceptionOutput.java
@@ -1,10 +1,10 @@
 package com.acelera.biblioteca.handler;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 public class FieldsExceptionOutput {
 
 	private String name;
